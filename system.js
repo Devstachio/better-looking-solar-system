d0e5696782065ad5d41c74e16d6b3578597fc0f1
Devstashio <63560701+TheMustachio@users.noreply.github.com>
2023-11-13 16:26:12 -0500

    added all planets up to mars

--- system.js
+++ system.js
@@ -41,10 +41,7 @@ loader.load(
     }
 );
 
-
-
-
-const tempLight = new THREE.AmbientLight(0xffffff, 2);
+const tempLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(tempLight);
 
 let earth = null;
@@ -56,7 +53,7 @@ loader.load(
         gltf.scene.scale.set(0.05, 0.05, 0.05);
 
         earth = gltf.scene;
-        earth.position.set(1000, 0, 0);
+        earth.position.set(5000, 0, 0);
 
 
         scene.add(gltf.scene);
@@ -76,7 +73,7 @@ loader.load(
         gltf.scene.scale.set(0.01, 0.01, 0.01);
 
         moon = gltf.scene;
-        moon.position.set(1000, 0, 0);
+        moon.position.set(5000, 0, 0);
 
 
         scene.add(gltf.scene);
@@ -87,10 +84,64 @@ loader.load(
     }
 );
 
+let mercury = null;
+loader.load(
+    '/models/mercury.glb', 
+    function (gltf) {
+        // Add the loaded model to the scene
+        gltf.scene.scale.set(0.05, 0.05, 0.05);
+
+        mercury = gltf.scene;
+        mercury.position.set(1000, 0, 0);
+        
+        scene.add(gltf.scene);
+    },
+    undefined,
+    function (error) {
+        console.error(error);
+    }
+);
+
+let venus = null;
+loader.load(
+    '/models/venus.glb',
+    function (gltf) {
+        // Add the loaded model to the scene
+        gltf.scene.scale.set(0.05, 0.05, 0.05);
+
+        venus = gltf.scene;
+        venus.position.set(2000, 0, 0);
+
+        scene.add(gltf.scene);
+    },
+    undefined,
+    function (error) {
+        console.error(error);
+    }
+);
+
+let mars = null;
+loader.load(
+    '/models/mars.glb',
+    function(gltf){
+        gltf.scene.scale.set(0.05, 0.05, 0.05);
+
+        mars = gltf.scene;
+        mars.position.set(4000, 0, 0);
+
+        scene.add(gltf.scene);
+    },
+    undefined, 
+    function(error){
+        console.error(error);
+    }
+);
+
+
 
 
 // add star
-//Array(200).fill().forEach(addStar);
+Array(100).fill().forEach(addStar);
 
 function addStar() {
     const star = new THREE.Mesh(new THREE.SphereGeometry(0.5, 25, 25), new THREE.MeshStandardMaterial({ color: 0xffffff }));
@@ -109,8 +160,12 @@ function addStar() {
     light.position.set(x, y, z);
     star.position.set(x, y, z);
 }
+
 let earthAngle = 0;
-let moonAngle = 0; 
+let moonAngle = 0;
+let mercuryAngle = 0;
+let venusAngle = 0;
+let marsAngle = 0;
 
 const animate = function () {
     requestAnimationFrame(animate);
@@ -125,9 +180,9 @@ const animate = function () {
     //make the planet orbit the sun
     if (earth) {
         earth.rotation.y += 0.1;
-        earthAngle -= 0.001;
-        earth.position.x = 1000 * Math.cos(earthAngle);
-        earth.position.z = 700 * Math.sin(earthAngle);
+        earthAngle -= 0.002;
+        earth.position.x = 3000 * Math.cos(earthAngle);
+        earth.position.z = 2700 * Math.sin(earthAngle);
     }
 
     if( moon){
@@ -143,6 +198,27 @@ const animate = function () {
             moon.position.z = 0 + 75 * Math.sin(moonAngle);
         }
         
+    }
+
+    if(mercury){
+        mercury.rotation.y += 0.03;
+        mercuryAngle -= 0.01;
+        mercury.position.x = 1000 * Math.cos(mercuryAngle);
+        mercury.position.z = 1000 * Math.sin(mercuryAngle);
+    }
+
+    if(venus){
+        venus.rotation.y += 0.003;
+        venusAngle -= 0.004;
+        venus.position.x = 2000 * Math.cos(venusAngle);
+        venus.position.z = 2000 * Math.sin(venusAngle);
+    }  
+    
+    if(mars){
+        mars.rotation.y += 0.009;
+        marsAngle -= 0.0019;
+        mars.position.x = 4000 * Math.cos(marsAngle);
+        mars.position.z = 4000 * Math.sin(marsAngle);
     }
 
 
